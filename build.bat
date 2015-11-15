7a7c12d9bc949eaaf6980c0096e273aff6e90227
Tim Hollies <tim@chapelhse588.freeserve.co.uk>
2015-11-15 13:32:29 +0000

    Added boost-nowide dependency and bugfixes in the build system

--- build.bat
+++
@@ -1,3 +0,0 @@
-call "%VS140COMNTOOLS%vsvars32.bat"
-cd "D:\TimTech\git\tmp\tmp2\fuzzy-database\build\boost_1_59_0"
-call "bootstrap.bat"
---
+++ build_modules/boost-nowide.cmake
@@ -0,0 +1,19 @@
+ExternalProject_Add(
+    nowide
+    URL http://cppcms.com/files/nowide/nowide_standalone.zip
+    # TIMEOUT 10
+    # # Force separate output paths for debug and release builds to allow easy
+    # # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
+    # CMAKE_ARGS -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
+    #            -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
+    #            -Dgtest_force_shared_crt=ON
+    # Disable install step
+    INSTALL_COMMAND ""
+    # Wrap download, configure and build steps in a script to log output
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON)
+
+ExternalProject_Get_Property(nowide install_dir)
+set(Boost_Nowide_INCLUDES "${install_dir}/src/nowide")
+set(Boost_Nowide_LIBRARIES "${install_dir}/src/nowide-build/Debug/libnowide.lib")
