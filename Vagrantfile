aff19def7e30fcbf85bd164e19a2a2881242c090
Martin Luk <i.am.martin@hotmail.com>
2015-11-17 19:50:49 +0000

    Vagrant install bii code cmake and pthread according to travis yml

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure(2) do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "hashicorp/precise32"
+  config.vm.box = "hashicorp/precise64"
 
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
--- scripts/vagrant_provisioning.sh
+++ scripts/vagrant_provisioning.sh
@@ -6,3 +6,12 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+apt-get install libpthread-stubs0-dev
+wget http://www.biicode.com/downloads/latest/ubuntu64
+mv ubuntu64 bii-ubuntu64.deb
+dpkg -i bii-ubuntu64.deb && apt-get -f install
+rm bii-ubuntu64.deb
+wget https://s3.amazonaws.com/biibinaries/thirdparty/cmake-3.0.2-Linux-64.tar.gz
+tar -xzf cmake-3.0.2-Linux-64.tar.gz
+cp -fR cmake-3.0.2-Linux-64/* /usr
+rm -rf cmake-3.0.2-Linux-64
