cb498d456f07917aac07c65f11cb18c7ac3e9173
Martin Luk <i.am.martin@hotmail.com>
2015-11-27 16:22:33 +0000

    User group permission

--- src/user/UserGroup.h
+++ src/user/UserGroup.h
@@ -2,7 +2,7 @@
 
 //UserGroup: Object that is superclass of all user permission
 //Three actions for each user group:view database, modify database, and user operaetion
-//Three groups of users: Guest, normal user (assumed to be academics), and admin
+//Three groups of users: Guest, editor user (assumed to be academics), and admin
 //
 
 class UserGroup {
@@ -18,4 +18,23 @@ class UserGroup {
 		bool modifyDbPerm;
 		bool userOpPerm;
 };
+
+class GuestGroup: public UserGroup {
+	//Guest can only view the DB
+	public: 
+		GuestGroup() : UserGroup(true, false, false){};
+};
+
+class EditorGroup: public UserGroup {
+	//Editor can edit and view DB
+	public: 
+		EditorGroup() : UserGroup(true, true, false){};
+};
+
+class UserAdminGroup: public UserGroup {
+	//User admin can only do user operations, without ability to view and edit DB
+	public: 
+		UserAdminGroup() : UserGroup(false, false, true){};
+};
+
 
