8e7ec440266b30656838ac19c2562b0936415709
Timothy Hollies <Timothy Hollies>
2015-12-23 11:46:23 +0000

    Replaced simple parser with a recursive decent parser

--- src/CommandInterpreter.cpp
+++ src/CommandInterpreter.cpp
@@ -1,90 +1,34 @@
-#define BOOST_SPIRIT_UNICODE
-
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix.hpp>
-
-#include <boost/fusion/adapted/struct/adapt_struct.hpp>
-#include <boost/fusion/include/adapt_struct.hpp>
-
-#include <vector>
-
 #include "./CommandInterpreter.h"
 
 #include "Job.h"
 #include "JobFactory.h"
 #include "JobQueue.h"
 
-struct command_struct
-{
-	std::string command;
-	std::string arg1;
-};
+#include "jobs/Ping.h"
+#include "jobs/Echo.h"
+#include "jobs/Unknown.h"
 
-BOOST_FUSION_ADAPT_STRUCT(
-	command_struct,
-	(std::string, command)
-	(std::string, arg1)
-	)
+#include "Parser.h"
 
-namespace client
-{
-	namespace qi = boost::spirit::qi;
+void CommandInterpreter::ProcessCommand(ISession* session, std::string command) {
 
-	template <typename Iterator>
-	struct command_parser : qi::grammar<Iterator, command_struct(), boost::spirit::unicode::space_type>
-	{
-		command_parser() : command_parser::base_type(start)
-		{
-			using qi::int_;
-			using qi::lit;
-			using qi::double_;
-			using qi::lexeme;
-			using boost::spirit::unicode::char_;
+	auto tokens = Tokenize(command);
 
-			string %= lexeme[+(char_ - ' ') | ('"' >> +(char_ - '"') >> '"')];
+	try {
+		Query query = ParseAll(tokens);
 
-			start %=
-				string
-				>> -string
-				;
-		}
-
-		qi::rule<Iterator, std::string(), boost::spirit::unicode::space_type> string;
-		qi::rule<Iterator, command_struct(), boost::spirit::unicode::space_type> start;
-	};
-}
-
-void CommandInterpreter::ProcessCommand(TCPSession* session, std::string command) {
-
-	client::command_parser<std::string::iterator> g;
-	command_struct cmd;
-	bool r = boost::spirit::qi::phrase_parse(command.begin(), command.end(), g, boost::spirit::unicode::space, cmd);
-	Job* job;
-
-	// Made this a bit more generic.
-	/*
-	if (r) {
-		if (cmd.arg1.length() == 0) {
-			job = JobFactory::createJob(session, cmd.command);
-		}
-		else {
-			job = JobFactory::createJob(session, cmd.command, cmd.arg1);
+		switch (query.type) {
+		case QueryType::PING:
+			JobQueue::AddJob(new PingJob(session));
+			break;
+		case QueryType::ECHO:
+			JobQueue::AddJob(new EchoJob(session, query.data0));
+			break;
+		default:
+			JobQueue::AddJob(new UnknownJob(session, command));
 		}
 	}
-	else {
-		job = JobFactory::createUnknownJob(session, cmd.command);
+	catch (ParseException ex) {
+		session->respond(std::string("Parse error: ") +  ex.what());
 	}
-	*/
-  //simple commands
-  
-	std::vector<std::string> args;
-	args.push_back(cmd.command);
-	if ( r )
-	{
-		if ( cmd.arg1.length() > 0 )
-			args.push_back(cmd.arg1);
-	}
-	job = JobFactory::createJob(session, args);
-
-  JobQueue::AddJob(job);
 }
