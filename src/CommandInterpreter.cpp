dd48dcb5a3e4493ccd0cbaf824a3dcbbaeeb8341
Timothy Hollies <Timothy Hollies>
2015-12-23 11:46:23 +0000

    Replaced simple parser with a recursive decent parser

--- src/CommandInterpreter.cpp
+++ src/CommandInterpreter.cpp
@@ -1,75 +1,34 @@
-#define BOOST_SPIRIT_UNICODE
-
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix.hpp>
-
-#include <boost/fusion/adapted/struct/adapt_struct.hpp>
-#include <boost/fusion/include/adapt_struct.hpp>
-
 #include "./CommandInterpreter.h"
 
 #include "Job.h"
 #include "JobFactory.h"
 #include "JobQueue.h"
 
-struct command_struct
-{
-	std::string command;
-	std::string arg1;
-};
+#include "jobs/Ping.h"
+#include "jobs/Echo.h"
+#include "jobs/Unknown.h"
 
-BOOST_FUSION_ADAPT_STRUCT(
-	command_struct,
-	(std::string, command)
-	(std::string, arg1)
-	)
-
-namespace client
-{
-	namespace qi = boost::spirit::qi;
-
-	template <typename Iterator>
-	struct command_parser : qi::grammar<Iterator, command_struct(), boost::spirit::unicode::space_type>
-	{
-		command_parser() : command_parser::base_type(start)
-		{
-			using qi::int_;
-			using qi::lit;
-			using qi::double_;
-			using qi::lexeme;
-			using boost::spirit::unicode::char_;
-
-			string %= lexeme[+(char_ - ' ') | ('"' >> +(char_ - '"') >> '"')];
-
-			start %=
-				string
-				>> -string
-				;
-		}
-
-		qi::rule<Iterator, std::string(), boost::spirit::unicode::space_type> string;
-		qi::rule<Iterator, command_struct(), boost::spirit::unicode::space_type> start;
-	};
-}
+#include "Parser.h"
 
 void CommandInterpreter::ProcessCommand(TCPSession* session, std::string command) {
 
-	client::command_parser<std::string::iterator> g;
-	command_struct cmd;
-	bool r = boost::spirit::qi::phrase_parse(command.begin(), command.end(), g, boost::spirit::unicode::space, cmd);
-	Job* job;
-	if (r) {
-		if (cmd.arg1.length() == 0) {
-			job = JobFactory::createJob(session, cmd.command);
-		}
-		else {
-			job = JobFactory::createJob(session, cmd.command, cmd.arg1);
+	auto tokens = Tokenize(command);
+
+	try {
+		Query query = ParseAll(tokens);
+
+		switch (query.type) {
+		case QueryType::PING:
+			JobQueue::AddJob(new PingJob(session));
+			break;
+		case QueryType::ECHO:
+			JobQueue::AddJob(new EchoJob(session, query.data0));
+			break;
+		default:
+			JobQueue::AddJob(new UnknownJob(session, command));
 		}
 	}
-	else {
-		job = JobFactory::createUnknownJob(session, cmd.command);
+	catch (ParseException ex) {
+		session->respond(std::string("Parse error: ") +  ex.what());
 	}
-  //simple commands
-   
-  JobQueue::AddJob(job);
 }
---
+++ src/Parser.h
@@ -0,0 +1,316 @@
+#ifndef FUZZY_PARSER
+#define FUZZY_PARSER
+
+#include <iterator>
+#include <vector>
+#include <string>
+
+using StringIterator = std::vector<std::string>::iterator;
+using StringMap = std::map<std::string, std::string>;
+
+std::vector<std::string> Tokenize(std::string str) {
+
+	std::vector<std::string> results;
+	std::string buffer;
+	bool speechMarks = false;
+	bool squareBrackets = false;
+	bool triangleBrackets = false;
+	bool filter = false;
+	bool typing = false;
+
+	for (auto iter = str.begin(); iter != str.end(); iter++) {	
+		if ((*iter == '\n' || *iter == ' ' || *iter == '\t') && !speechMarks && !squareBrackets && !triangleBrackets && !filter) {
+			if (buffer.length() > 0) {
+				results.push_back(buffer);
+				buffer.clear();
+			}
+			typing = false;
+		}
+		else {
+
+			if ((*iter == '{' || *iter == '}' || *iter == ';' || *iter == ',' || *iter == '.' || (*iter == ':' && typing == false)) && !speechMarks && !squareBrackets && !filter) {
+				if (buffer.length() > 0) {
+					results.push_back(buffer);
+					buffer.clear();
+				}
+				buffer.push_back(*iter);
+				results.push_back(buffer);
+				buffer.clear();
+			} else {
+
+				buffer += *iter;
+
+				if (*iter == '^') {
+					typing = true;
+				}
+
+				if (buffer == "FILTER(") {
+					filter = true;
+				}
+
+				if (filter && *iter == ')') {
+					filter = false;
+				}
+
+				if (*iter == '"') {
+					speechMarks = !speechMarks;
+				}
+
+				if (*iter == '[' && !squareBrackets) {
+					squareBrackets = true;
+				}
+
+				if (*iter == ']' && squareBrackets) {
+					squareBrackets = false;
+				}
+
+				if (*iter == '<' && !triangleBrackets) {
+					triangleBrackets = true;
+				}
+
+				if (*iter == '>' && triangleBrackets) {
+					triangleBrackets = false;
+				}
+
+			}
+			
+		}
+	}
+
+	if (buffer.length() > 0) {
+		results.push_back(buffer);
+		buffer.clear();
+	}
+
+	return results;
+}
+
+class ParseException : public std::exception {
+
+private:
+
+	std::string _message;
+
+public:
+	
+	ParseException(std::string msg) {
+		_message = msg;
+	}
+
+	virtual const char* what() const throw()
+	{
+		return _message.c_str();
+	}
+};
+
+struct Triple {
+public: 
+	std::string subject;
+	std::string predicate;
+	std::string object;
+
+	Triple(std::string sub, std::string pred, std::string obj) {
+		subject = sub;
+		predicate = pred;
+		object = obj;
+	}
+};
+
+struct TriplesBlock {
+public:
+	std::vector<Triple> triples;
+	std::string name;
+	std::vector<std::string> filters;
+
+	TriplesBlock(std::vector<Triple> trip, std::string n) {
+		triples = trip;
+		name = n;
+	}
+
+	TriplesBlock(std::vector<Triple> trip) {
+		triples = trip;
+	}
+
+	TriplesBlock() {}
+};
+
+enum class QueryType {
+	SELECT,
+	INSERT,
+	DEL,
+	PING,
+	ECHO,
+	USER
+};
+
+struct Query {
+public:
+
+	QueryType type;
+	StringMap sources;
+	TriplesBlock conditions;
+	TriplesBlock whereClause;
+	std::string data0;
+
+	Query(QueryType t, StringMap s, TriplesBlock cond, TriplesBlock wh, std::string dat0) {
+		type = t;
+		sources = s;
+		conditions = cond;
+		whereClause = wh;
+		data0 = dat0;
+	}
+};
+
+std::vector<Triple> ParseTriples(StringIterator&& iter, StringIterator end) {
+	std::vector<Triple> triples;
+	std::string sub;
+	std::string pred;
+	int pos = 0;
+
+	while(iter != end && *iter != "}") {
+
+		if (*iter != ";" && *iter != "," && *iter != ".") {
+			switch (pos) {
+			case 0: 
+				sub = *iter;
+				pos = 1;
+				break;
+			case 1:
+				pred = *iter;
+				pos = 2;
+				break;
+			case 2:
+				Triple trip(sub, pred, *iter);
+				triples.push_back(trip);
+				break;
+			}
+		}
+		else {
+
+			if (*iter == ".") {
+				pos = 0;
+			}
+
+			if (*iter == ";") {
+				pos = 1;
+			}
+
+			if (*iter == ",") {
+				pos = 2;
+			}
+		}
+
+		iter++;
+	}
+
+	return triples;
+}
+
+TriplesBlock ParseInsert(StringIterator&& iter, StringIterator end) {
+
+	TriplesBlock output;
+
+	if (*iter == "{") {
+		iter++;
+		output = TriplesBlock(ParseTriples(std::move(iter), end));
+
+		if (*iter == "}") {
+			iter++;
+		}
+	}
+
+	return output;
+}
+
+StringMap ParseSources(StringIterator&& iter, StringIterator end) {
+	
+	StringMap sources;
+
+	while (iter != end && *iter != "INSERT" &&  *iter != "SELECT" && *iter != "DELETE") {
+
+		std::string name;
+
+		name = *iter;
+		iter++;
+
+		if (iter != end) {
+			if (*iter == ":") {
+				iter++;
+				if (iter != end) {
+					sources[name] = *iter;
+					iter++;
+				}
+			}
+		}
+	}
+
+	return sources;
+}
+
+Query ParseAll(std::vector<std::string> tokens) {
+
+	auto iter = tokens.begin();
+
+	QueryType type;
+	StringMap sources;
+	TriplesBlock conditions;
+	TriplesBlock whereClause;
+	std::string data0;
+
+	while(iter != tokens.end()) {
+			
+		if (*iter == "SOURCE") {
+			*iter++;
+			sources = ParseSources(std::move(iter), tokens.end());
+			continue;
+		}
+
+		if (*iter == "INSERT") {
+			*iter++;
+			if (*iter == "DATA") {
+				iter++;
+				type = QueryType::INSERT;
+				conditions = ParseInsert(std::move(iter), tokens.end());
+			}
+			continue;
+		}
+
+		if (*iter == "WHERE") {
+			*iter++;
+			whereClause = ParseInsert(std::move(iter), tokens.end());
+			continue;
+		}
+
+		if (*iter == "PING") {
+			*iter++;
+			type = QueryType::PING;
+			
+			if (iter != tokens.end()) {
+				throw ParseException("PING does not take any arguments");
+			}
+
+			break;
+		}
+
+		if (*iter == "ECHO") {
+			*iter++;
+			type = QueryType::ECHO;
+
+			if (iter != tokens.end()) {
+				data0 = *iter;
+				*iter++;
+				if (iter != tokens.end()) {
+					throw ParseException("ECHO only takes one argument");
+				}
+			}
+
+			break;
+		}
+		
+		throw ParseException("Unknown symbol: " + *iter);
+	}
+
+	return Query(type, sources, conditions, whereClause, data0);
+}
+
+#endif
