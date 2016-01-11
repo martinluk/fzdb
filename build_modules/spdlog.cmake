a427ae0ff589683ed9afa1f5488180243f5f3d7d
TimHollies <t.hollies@warwick.ac.uk>
2016-01-11 13:19:03 +0000

    Minor fix to allow project to build on DCS machines again

--- build_modules/spdlog.cmake
+++ build_modules/spdlog.cmake
@@ -4,6 +4,8 @@ ExternalProject_Add(
     DOWNLOAD_DIR external
     DOWNLOAD_NAME spdlog.zip
     INSTALL_COMMAND ""
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
     LOG_DOWNLOAD ON
     LOG_CONFIGURE ON
     LOG_BUILD ON)
