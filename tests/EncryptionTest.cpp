a3e5b0c681a19a118a601ee70b6647cabcb4cadb
Martin Luk <i.am.martin@hotmail.com>
2016-01-14 19:51:15 +0000

    sha1

---
+++ tests/EncryptionTest.cpp
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include "gtest/gtest.h"
+#include "user/Encryption.h"
+
+class EncryptionTest : public ::testing::Test {
+protected:
+	virtual void setUp() {
+
+	}
+	virtual void TearDown() {
+
+	}
+};
+
+TEST_F(EncryptionTest, hash) {
+	Encryption e;
+	std::string hash = e.hash("pw","salt");
+	EXPECT_STREQ(hash.c_str(),"2bfa821dd4f3af5adfb5734457839bed7b324819");
+}
+
