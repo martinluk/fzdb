656c45ca2de7f5303c9915e137b96bf0198fab0e
Martin Luk <i.am.martin@hotmail.com>
2016-01-21 20:13:51 +0000

    Being expilict with conditions

--- src/user/UserLogin.cpp
+++ src/user/UserLogin.cpp
@@ -11,23 +11,28 @@ void UserFileOperations::addUser(UserAttributes userAttributes) {
 	loadCacheFromFile();
 	//Assert that no such user already exist, otherwise throw exception
 	std::string newUserName=userAttributes.userName;
-	if (this->userFileCache.count(newUserName)) {
+	if (this->userFileCache.count(newUserName)==0) {
 		throw new UserAlreadyExistException;
 	}
 	//Add into cache
-	//userFileCache[newUserName]=userAttributes;
+	userFileCache[newUserName]=userAttributes;
 	//save cache from file
 	saveCacheToFile();
 }
 void UserFileOperations::removeUser(std::string userName) {
 	//load cache from file?
 	loadCacheFromFile();
+	if((this->userFileCache.count(userName))>0) {
+		throw new UserNotExistException;
+	}
+	//TODO Remove the entry
 	//save cache from file?
 	saveCacheToFile();
 }
 void UserFileOperations::updateUser(std::string userName, UserAttributes newAttributes) {
 	//load cache from file?
 	loadCacheFromFile();
+	//TODO Update user attribute at 
 	//save cache from file?
 	saveCacheToFile();
 }
