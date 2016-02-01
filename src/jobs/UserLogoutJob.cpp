90d41188135c38d8e243b0c3204a053dc6b233df
Martin Luk <i.am.martin@hotmail.com>
2016-02-01 00:40:48 +0000

    Forgot to add the actual files...

---
+++ src/jobs/UserLogoutJob.cpp
@@ -0,0 +1,16 @@
+#include "UserLogoutJob.h"
+
+//User folder includes
+#include "../user/Permission.h"
+#include "../user/UserOperation.h"
+
+UserLogoutJob::UserLogoutJob(ISession* session) : Job(session) {
+	_session=session;
+};
+
+QueryResult UserLogoutJob::execute() {
+	QueryResult result;
+	//TODO Some kind of exception of user is not already login?
+	_session->clearCurrentUserName();
+	return result;
+}
