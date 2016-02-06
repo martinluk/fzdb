4b976a9afc8e89d37297b41194f45e5048fec5f6
X6Herbius <jonathan.poncelet@talk21.com>
2016-02-06 13:45:55 +0000

    Fixed typo making build fail :c

--- src/Util.h
+++ src/Util.h
@@ -8,7 +8,7 @@ namespace util
 {
     std::vector<std::string> split(const std::string &s, char delim);
 		std::string toUppercase(const std::string &str);
-		std::String toLowercase(const std::string &str);
+		std::string toLowercase(const std::string &str);
 }
 
 #endif  // UTIL_H
