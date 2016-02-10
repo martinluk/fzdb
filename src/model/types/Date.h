95d7e95c19dea5ceb580feaf8611c1853a9f41e9
X6Herbius <jonathan.poncelet@talk21.com>
2016-02-10 10:32:55 +0000

    Added date conversion functions

--- src/model/types/Date.h
+++ src/model/types/Date.h
@@ -21,6 +21,44 @@ namespace model
 			}
 
 		public:
+			/**
+			*   Encodes a date
+			*
+			*   @param int y The year of the date to be encoded
+			*   @param int m The month of the date to be encoded
+			*   @param int d The day of the date to be encoded
+			*   @return int The encoded date
+			*/
+			static int Encode(int y, int m, int d)
+			{
+			    m = (m + 9) % 12;
+			    y = y - (m/10);
+			    return (365 * y) + (y/4) - (y/100) + (y/400) + ((m*306 + 5) / 10) + (d-1);
+			}
+			
+			/**
+			*   Decodes a date
+			*
+			*   @param int g An encoded date
+			*   @param int y Year output
+			*	@param int mm Month output
+			*	@prarm int dd Day output
+			*/
+			static void Decode(int g, int &y, int &mm, int &dd)
+			{
+			    y = ((10000*g + 14780)/3652425);
+			    int ddd = g - (365*y + floor(y/4) - (y/100) + (y/400));
+			    if (ddd < 0)
+				{
+			        y = y - 1;
+			        ddd = g - (365*y + (y/4) - (y/100) + (y/400));
+			    }
+			    int mi = ((100*ddd + 52)/3060);
+			    mm = (mi + 2)%12 + 1;
+			    y = y + ((mi + 2)/12);
+			    dd = ddd - ((mi*306 + 5)/10) + 1;
+			}
+			
 			Date() : _value(0), Base(100)
 			{
 				initMemberSerialiser();
