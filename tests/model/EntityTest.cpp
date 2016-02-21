f897b8b1a6c83b6ad34901bd7466815c7f326033
Martin Luk <i.am.martin@hotmail.com>
2016-02-21 14:41:48 +0000

    Organised set up tests into folder

---
+++ tests/model/EntityTest.cpp
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include "gtest/gtest.h"
+#include "model/Entity.h"
+
+class EntityTest : public ::testing::Test {
+protected:
+	virtual void setUp() {
+
+	}
+	virtual void TearDown() {
+
+	}
+};
+
+TEST_F(EntityTest, cube) {
+	//FAIL();
+}
