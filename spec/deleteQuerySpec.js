9f11be50d414bd89125011f9c30c34d5712209d7
Martin Luk <i.am.martin@hotmail.com>
2016-03-06 11:57:49 +0000

    Tidied up sdelete query spec a little bit

--- spec/deleteQuerySpec.js
+++ spec/deleteQuerySpec.js
@@ -1,7 +1,7 @@
 var net = require('net');
 var Promise = require("es6-promise").Promise
 
-describe("fzdb:delete queries", function() {
+describe("fzdb", function() {
   var client;
 
   //connects to the database
@@ -17,48 +17,42 @@ describe("fzdb:delete queries", function() {
 
   var sendCmd = function(cmd) {
     client.write(cmd);
-    return new Promise(function(resolve, reject) {
-      client.once('data', function(data) {
-        resolve(JSON.parse(data));
-      }); 
-    });     
-  }
-  
-  describe("Normal use case", function() {
+	return new Promise(function(resolve, reject) {
+		client.once('data', function(data) {
+		resolve(JSON.parse(data));
+		}); 
+	});     
+	}
 
-    it("Asserting there was no one called Fred in the DB", function(done) {
-      sendCmd("SELECT $a WHERE { $a <forename> \"Fred\" }")
-      .then(function(data) {
-			expect((data.result.data).length).toBe(0);
-        done();
-      });          
-    });
-    it("Adding Fred into DB", function(done) {
-      sendCmd("INSERT DATA { entity:1 <forename> \"Fred\" }")
-      .then(function(data) {
-        done();
-      });    
-    });
-    it("Asserting Fred now lives in DB", function(done) {
-      sendCmd("SELECT $a WHERE { $a <forename> \"Fred\" }")
-      .then(function(data) {
-			expect((data.result.data).length).toBe(1);
-        done();
-      });          
-    });
-
-    it("Deleting Fred from DB", function(done) {
-      sendCmd("DELETE DATA { entity:1 <forename> \"Fred\" }")
-      .then(function(data) {
-        done();
-      });    
-    });
-    it("Asserting Fred no longer in DB", function(done) {
-      sendCmd("SELECT $a WHERE { $a <forename> \"Fred\" }")
-      .then(function(data) {
-			expect((data.result.data).length).toBe(0);
-        done();
-      });          
-    });
-  });
+	describe("delete queries:", function() {
+		describe("DB with one entity", function() {
+			it("no one used to call Fred", function(done) {
+				sendCmd("SELECT $a WHERE { $a <forename> \"Fred\" }").then(function(data) {
+					expect((data.result.data).length).toBe(0); done();
+				});          
+			});
+			it("Adding Fred into DB", function(done) {
+				sendCmd("INSERT DATA { entity:1 <forename> \"Fred\" }").then(function(data) {
+					done(); 
+				});    
+			});
+			it("and now Fred lives in DB", function(done) {
+				sendCmd("SELECT $a WHERE { $a <forename> \"Fred\" }").then(function(data) {
+					expect((data.result.data).length).toBe(1);
+					done();
+				});          
+			});
+			it("Running the delete query", function(done) {
+				sendCmd("DELETE DATA { entity:1 <forename> \"Fred\" }").then(function(data) {
+					done();
+				});    
+			});
+			it("Resulting in Fred no longer lives in DB", function(done) {
+				sendCmd("SELECT $a WHERE { $a <forename> \"Fred\" }").then(function(data) {
+					expect((data.result.data).length).toBe(0);
+					done();
+				});          
+			});
+		});
+	});
 });
