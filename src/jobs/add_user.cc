b19c293b4c5781097010812bed7509f2b0ff9f3c
Martin Luk <i.am.martin@hotmail.com>
2016-04-28 21:12:41 +0100

    corrected spelling mistake

--- src/jobs/add_user.cc
+++ src/jobs/add_user.cc
@@ -21,6 +21,6 @@ QueryResult AddUser::executeNonConst() {
   }
 
   QueryResult result;
-  result.setResultDataText(std::string("User ") + _username + std::string(" successfulyl added."));
+  result.setResultDataText(std::string("User ") + _username + std::string(" successfully added."));
   return result;
 }
