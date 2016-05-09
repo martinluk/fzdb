a09e0eabb4d42cd76bc4028f8cc95d9b0c84b3ae
root <root@bzdbbb.co.uk>
2016-05-09 06:21:18 -0400

    linux script updates

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -2,11 +2,11 @@
 cp build/fuzzy-database /bin
 #copy init.d script to the right place
 cp scripts/linux_daemon.sh /etc/init.d
-mv /etc/init.d/scripts/linux_daemon.sh /etc/init.d/scripts/FZDB
+mv /etc/init.d/linux_daemon.sh /etc/init.d/FZDB
 #set permissons on init.d script
-chmod uga+x /etc/init.d/scripts/FZDB
-#database config file - /etc/fzdb/config.cnf
-mdkir /etc/fzdb
+chmod uga+x /etc/init.d/FZDB
+#6database config file - /etc/fzdb/config.cnf
+mkdir -p /etc/fzdb
 cp scripts/linux_config.cnf /etc/fzdb
 mv /etc/fzdb/linux_config.cnf /etc/fzdb/default.cnf
 #set to start at system start
@@ -14,4 +14,4 @@ update-rc.d FZDB defaults
 #touch log file
 touch /var/log/FZDB.log
 #mkdir data/fzdb
-mkdir /data/fzdb
+mkdir -p /data/fzdb
